98204cdace03d62e4df33d1a9a91de69f209ddc1
raywu6 <rwu6@stuy.edu>
2019-01-08 23:16:13 -0500

    consolidate 2 database-working .py files

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template, session, redirect, flash, url_for #pip install flask
-
 from urllib import request, error #stdlib
 import os
+
+from flask import Flask, render_template, session, redirect, flash, url_for #pip install flask
 from flask import request as frequest
 
-
-from util import database, db
+from util import database
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
@@ -39,7 +38,7 @@ def authenticate():
     registering. It will redirect the user to the appropriate pages and flash
     messages if there are errors.'''
     submit_type = frequest.form.get("submit")
-    username_list = db.get_username_list()
+    username_list = database.get_username_list()
     if (submit_type == "Register"):
         username_input = frequest.form.get("username")
         password_input = frequest.form.get("password")
@@ -54,7 +53,7 @@ def authenticate():
         elif (password_input != c_password_input):
             flash("Password and Confirm Password do not match.","error")
         else:
-            db.add_user(username_input,password_input)
+            database.add_user(username_input,password_input)
             flash("Successfully created account.","success")
             return redirect(url_for("login"))
         return redirect(url_for("register"))
@@ -63,8 +62,8 @@ def authenticate():
         username_input = frequest.form.get("username")
         password_input = frequest.form.get("password")
         if (username_input in username_list):
-            if (db.check_password(username_input,password_input)):
-                session["id"] = db.get_id_from_username(username_input)
+            if (database.check_password(username_input,password_input)):
+                session["id"] = database.get_id_from_username(username_input)
                 return redirect(url_for("home"))
         flash("Username or password is incorrect. Please try again.","error")
         return redirect(url_for("login"))

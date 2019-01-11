74f0bd96a551ed59425b260b7754df02cc986e76
raywu6 <rwu6@stuy.edu>
2019-01-11 13:02:26 -0500

    remove fortune b/c it takes too long for page to load

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os #stdlib
 
 from flask import Flask, render_template, session, redirect, request, flash, url_for #pip install flask
 
-from util import database, googleCivicInfo, news_api, fortune
+from util import database, googleCivicInfo, news_api
 
 import pprint
 
@@ -13,11 +13,11 @@ app.secret_key = os.urandom(32)
 @app.route('/')
 def home():
     #print(news_api.news_api("W"))
-    pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(googleCivicInfo.civic(10027))
+    # pp = pprint.PrettyPrinter(indent=4)
+    # pp.pprint(googleCivicInfo.civic(10027))
     news_list = news_api.nyt_news("china")
-    quote = fortune.getQuote()
-    return render_template("index.html", q = quote, l = news_list, c = len(news_list))
+    # quote = fortune.getQuote()
+    return render_template("index.html", l = news_list, c = len(news_list))
 
 def is_logged_in():
     '''Returns True if the user is logged in. False otherwise.'''
